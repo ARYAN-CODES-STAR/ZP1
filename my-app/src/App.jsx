8cb42837315da99b50927c6fc6736d6fe8e804d6
ARYAN-CODES-STAR <aryansh0004@gmail.com>
2023-01-20 23:43:21 +0530

    Added component Heading and changed file structure

---
+++ my-app/src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <>
+
+      <div>
+        <h1>Aryan </h1>
+        <h2>is</h2>
+        <h1>great</h1>
+      </div>
+      
+    </>
+  );
+}
+
+
+export default App;

f0fd3ba51b2401795859f4939e09e8e5599ebb86
ARYAN-CODES-STAR <aryansh0004@gmail.com>
2023-01-21 23:08:14 +0530

    added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,22 +1,25 @@
 import MyHeading from "./components/MyHeading";
 
+
 function App() {
+  let a = 7;
+
   return (
-   <>
-       
+    <>
+      <p>the variable here used is {a} </p>
       <div>
-      <MyHeading />
+        {/* <b />
+        <c />
+        <d /> */}
+        <MyHeading />
+       
 
         <h1>Aryan </h1>
         <h2>is</h2>
         <h1>great</h1>
       </div>
- 
-
-    </>  
-     
+    </>
   );
 }
-
 
 export default App;
--- my-app/src/components/MyHeading.js
+++ my-app/src/components/MyHeading.js
@@ -1,15 +1,23 @@
-import React from 'react'
+import React from "react";
 
 const MyHeading = () => {
-    const a=5;
+  const a = 5;
   return (
     <div>
-        {/* <p>this is para </p> */}
-        <h2>This rendered compo</h2>
-        <p>lemme use this {a}</p>
+      <h2>This rendered compo</h2>
+      <p>lemme use this {a}</p>
     </div>
-    
-  )
-}
+  );
+};
+
+// export const b = () => {
+//   <p> this is b in para</p>;
+// };
+// export const c = () => {
+//   <p> this is c in para</p>;
+// };
+// export const d = () => {
+//   <p> this is d in para</p>;
+// };
 
 export default MyHeading;

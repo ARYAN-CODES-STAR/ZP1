01c3033c2787e61836cb75f63c3e403801ffe893
ARYAN-CODES-STAR <aryansh0004@gmail.com>
2023-01-21 21:46:05 +0530

    Added a MyHeading component and created components folder

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,20 @@
+import MyHeading from "./components/MyHeading";
+
 function App() {
   return (
-    <>
-
+   <>
+       
       <div>
+      <MyHeading />
+
         <h1>Aryan </h1>
         <h2>is</h2>
         <h1>great</h1>
       </div>
-      
-    </>
+ 
+
+    </>  
+     
   );
 }
 
---
+++ my-app/src/components/MyHeading.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const MyHeading = () => {
+    const a=5;
+  return (
+    <div>
+        {/* <p>this is para </p> */}
+        <h2>This rendered compo</h2>
+        <p>lemme use this {a}</p>
+    </div>
+    
+  )
+}
+
+export default MyHeading;
